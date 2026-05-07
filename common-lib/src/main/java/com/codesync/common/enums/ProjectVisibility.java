514a725c6236dc32f1951524a34653ca2ecb95ca
wasim-akaram <wasimakaram360@gmail.com>
2026-05-07 14:47:57 +0530

    [Wasim]: Removed files from remote branch

--- common-lib/src/main/java/com/codesync/common/enums/ProjectVisibility.java
+++
@@ -1,9 +0,0 @@
-/*
- * Code reader note: Lists the visibility values used to decide whether projects are public or private.
- */
-package com.codesync.common.enums;
-
-public enum ProjectVisibility {
-    PUBLIC,
-    PRIVATE
-}
